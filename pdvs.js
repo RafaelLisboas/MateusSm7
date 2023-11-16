0772b78710a86863555bd263bf0bf6e9abd84bd8
RafaelLisboas <150970003+RafaelLisboas@users.noreply.github.com>
2023-11-16 18:06:39 -0300

    Add files via upload

---
+++ pdvs.js
@@ -0,0 +1,331 @@
+let pdvsJson = [ 
+    {
+        name: 'PDV 1',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.53:9898/normal.html'
+    },
+    {
+        name: 'PDV 2',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.147:9898/normal.html'
+    },
+    {
+        name: 'PDV 3',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.163:9898/normal.html'
+    },
+    {
+        name: 'PDV 4',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.164:9898/normal.html'
+    },
+    {
+        name: 'PDV 5',
+        descripion:'Super Drive',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.165:9898/normal.html'
+    },
+    {
+        name: 'PDV 6',
+        descripion:'Super Drive',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.166:9898/normal.html'
+    },
+    {
+        name: 'PDV 7',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.24:9898/normal.html'
+    },
+    {
+        name: 'PDV 8',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.222:9898/normal.html'
+    },
+    {
+        name: 'PDV 9',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.180:9898/normal.html'
+    },
+    {
+        name: 'PDV 10',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.170:9898/normal.html'
+    },
+    {
+        name: 'PDV 11',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.111:9898/normal.html'
+    },
+    {
+        name: 'PDV 12',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.127:9898/normal.html'
+    },
+    {
+        name: 'PDV 13',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.110:9898/normal.html'
+    },
+    {
+        name: 'PDV 14',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.114:9898/normal.html'
+    },
+    {
+        name: 'PDV 15',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.115:9898/normal.html'
+    },
+    {
+        name: 'PDV 16',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.190:9898/normal.html'
+    },
+    {
+        name: 'PDV 17',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.25:9898/normal.html'
+    },
+    {
+        name: 'PDV 18',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.49:9898/normal.html'
+    },
+    {
+        name: 'PDV 19',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.119:9898/normal.html'
+    },
+    {
+        name: 'PDV 20',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.34:9898/normal.html'
+    },
+    {
+        name: 'PDV 21',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.118:9898/normal.html'
+    },
+    {
+        name: 'PDV 22',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.137:9898/normal.html'
+    },
+    {
+        name: 'PDV 23',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.174:9898/normal.html'
+    },
+    {
+        name: 'PDV 24',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.22:9898/normal.html'
+    },
+    {
+        name: 'PDV 01',
+        descripion:'ELETRO',
+        img: 'imagens//ELETRO_MATEUS.png',
+        link: 'http://192.168.60.71:9898/normal.html'
+    },
+    {
+        name: 'PDV 02',
+        descripion:'ELETRO',
+        img: 'imagens/ELETRO_MATEUS.png',
+        link: 'http://192.168.60.145:9898/normal.html'
+    },
+     {
+        name: 'PDV 03 - Extra',
+        descripion:'ELETRO',
+        img: 'imagens/ELETRO_MATEUS.png',
+        link: 'http://192.168.60.160:9898/normal.html'
+    },
+    {
+        name: 'PDV 01',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.178:9898/normal.html'
+    },
+    {
+        name: 'PDV 02',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.112:9898/normal.html'
+    },
+    {
+        name: 'PDV 03',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.187:9898/normal.html'
+    },
+    {
+        name: 'PDV 04',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.188:9898/normal.html'
+    },
+    {
+        name: 'PDV 05',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.181:9898/normal.html'
+    },
+    {
+        name: 'PDV 06',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.171:9898/normal.html'
+    },
+    {
+        name: 'PDV 07',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.183:9898/normal.html'
+    },
+    {
+        name: 'PDV 08',
+        descripion:'Rotisseria',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.191:9898/normal.html'
+    },
+    
+    {
+        name: 'PDV 09',
+        descripion:'A La Carte',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.184:9898/normal.html'
+    },
+    
+    {
+        name: 'PDV 10',
+        descripion:'A La Carte',
+        img: 'imagens/spazio.jpg',
+        link: 'http://192.168.60.141:9898/normal.html'
+    },
+    {
+        name: 'PDV - CAFÉ',
+        descripion:'Café',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.121:9898/normal.html'
+    },
+    {
+        name: 'PDV - ADEGA',
+        descripion:'Adega',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.102:9898/normal.html'
+    },
+    
+    {
+        name: 'PDV - Churros',
+        descripion:'Churros',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.155:9898/normal.html'
+    },
+     {
+        name: 'PDV 01',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.131:9898/normal.html'
+    },
+    {
+        name: 'PDV 02',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.221:9898/normal.html'
+    },
+    {
+        name: 'PDV 03',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.133:9898/normal.html'
+    },
+    {
+        name: 'PDV 04',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.134:9898/normal.html'
+    },
+    {
+        name: 'PDV 05',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.169:9898/normal.html'
+    },
+    {
+        name: 'PDV 06',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.29:9898/normal.html'
+    },
+    {
+        name: 'PDV 07',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.173:9898/normal.html'
+    },
+    {
+        name: 'PDV 08',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.136:9898/normal.html'
+    },
+    {
+        name: 'PDV 09',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.132:9898/normal.html'
+    },
+    {
+        name: 'PDV 10',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.139:9898/normal.html'
+    },
+    {
+        name: 'PDV 11',
+        descripion:'Area Nova',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.217:9898/normal.html'
+    },
+    {
+        name: 'PDV - app',
+        descripion:'Boteco',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.116:9898/normal.html'
+    },
+    {
+        name: 'PDV 01',
+        descripion:'Boteco',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.177:9898/normal.html'
+    },
+    {
+        name: 'PDV 02',
+        descripion:'Boteco',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.46:9898/normal.html'
+    },
+    
+    
+];
